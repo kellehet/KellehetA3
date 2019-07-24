4f1841c0c549e4a196da36208a62dde9f377c9b6
kellehet <31115855+kellehet@users.noreply.github.com>
2019-07-24 11:40:51 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,10 @@
+import { SongInfo } from '../../app/songInfo';
+
+export const MYSONGS: SongInfo[] = [
+    { songName: 'School', artist: 'Supertramp', genre: 'Classic Rock', yearReleased: 1974, image: "school.jpg" },
+    { songName: 'Radio GaGa', artist: 'Queen', genre: 'Classic Rock', yearReleased: 1975, image: "Bohemian_Rhapsody.jpg" },
+    { songName: 'Kashmir', artist: 'Led Zeppelin', genre: 'Classic Rock', yearReleased: 1971, image: "stairway_to_heaven.jpg" },
+    { songName: 'Free Bird', artist: 'Lynyrd Skynyrd', genre: 'Classic Rock', yearReleased: 1973, image: "freebird.jpg" }
+
+]
+
